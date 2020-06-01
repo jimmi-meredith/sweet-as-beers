9771b61fbd6dfd53ad04694d58c4cddcc98095a6
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-02 10:53:48 +1200

    copy beer list info into BeerList component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Header from './Header'
 
+import beers from '../../data/beers'
+
 // This might need to be turned into a stateful (class-based) component
 const App = () => (
   <div className='app'>
--- client/components/BeerList.jsx
+++ client/components/BeerList.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
 
-const BeerList = () => {
+const BeerList = props => {
+  return (
+    <div className='beerlist'>
+      <p className='welcome'>
+        Welcome! Please select from our delicious selection and don't hesitate to let us know if we can answer any of your questions.
+      </p>
 
+      <div className="beer">
+        <p className="name">HBIB Ginger Fusion</p>
+        <p className="description">A Ginger Beer from Hawkes Bay Independent Brewery</p>
+        <p>
+          <span className="country">New Zealand</span>
+          <span className="abv">5% abv</span>
+          <a href="/designs/cart.html" className="cart-link">Add to cart</a>
+        </p>
+      </div>
+
+      <div className="beer">
+        <p className="name">Mangose & Melons</p>
+        <p className="description">A Fruit Gose from Baylands Brewery</p>
+        <p>
+          <span className="country">New Zealand</span>
+          <span className="abv">4% abv</span>
+          <a href="/designs/cart.html" className="cart-link">Add to cart</a>
+        </p>
+      </div>
+
+      <div className="beer">
+        <p className="name">North End Pilsner</p>
+        <p className="description">A Pilsner from North End Brewing</p>
+        <p>
+          <span className="country">New Zealand</span>
+          <span className="abv">5% abv</span>
+          <a href="/designs/cart.html" className="cart-link">Add to cart</a>
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default BeerList
