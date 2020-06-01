e469724f72a83437628bb017b05a2c7b44349fe0
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-02 10:27:13 +1200

    Add comments on index.js for store setup

--- client/index.js
+++ client/index.js
@@ -1,17 +1,23 @@
 import React from 'react'
-import {render} from 'react-dom'
-import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+// importing the create store function for from redux
+// First step for redux and super important
+import { createStore } from 'redux'
 
+// importing all of the reducers from reducers file
 import reducers from './reducers'
 import App from './components/App'
 
+// Creating the store function, which passes the reducers from above
 const store = createStore(reducers,
+  // this is how we wire in the devtools in the browser (also important)
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
+    // passing the sotre as props into provider
     <Provider store={store}>
       <App />
     </Provider>,
