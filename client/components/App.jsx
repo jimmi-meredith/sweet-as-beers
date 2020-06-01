348b0ec69337d7f4f174cf1aa19efb4e3bf7defa
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-02 09:50:09 +1200

    Import Header into App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import Header from './Header'
+
 // This might need to be turned into a stateful (class-based) component
 const App = () => (
   <div className='app'>
-    Ready to rock and roll
+    <Header />
   </div>
 )
 
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Header = () => {
-
+  return (
+    <h1>
+      <span className='fa fa-beer'></span> Sweet As Beers <span className='fa fa-beer'></span>
+    </h1>
+  )
 }
 
 export default Header
