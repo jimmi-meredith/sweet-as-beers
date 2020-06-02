6396465fb350e3287b111dd4fb10885d3bb1a1de
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-02 16:29:00 +1200

    Add basic info for CartItem.jsx

--- client/components/CartItem.jsx
+++ client/components/CartItem.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const CartItem = props => {
-
+  return (
+    <tr>
+      <td>HBIB Ginger Fusion</td>
+      <td><input className='update-input' value="3" /></td>
+      <td><button><span className='fa fa-trash fa-2x'></span></button></td>
+    </tr>
+  )
 }
 
 export default CartItem
