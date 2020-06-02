b1679efa5fdb2056a1987b0ad50fcf985c8a656f
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-02 15:02:26 +1200

    Convert BeerListItem to use props

--- client/components/BeerListItem.jsx
+++ client/components/BeerListItem.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const BeerListItem = props => {
-
-
+  const beer = props.beer
+  return (
+    <div className='beer'>
+      <p className='name'>{beer.name}</p>
+      <p className='description'>A {beer.style} from {beer.brewery}</p>
+      <p>
+        <span className='country'>{beer.country}</span>
+        <span className='abv'>{beer.abv} abv</span>
+        <a href='/designs/cart.html' className='cart-link'>Add to cart</a>
+      </p>
+    </div>
+  )
 }
 
 export default BeerListItem
