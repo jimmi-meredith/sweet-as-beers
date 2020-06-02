2cc90d24120ec057a8310cd24bc7f18a55910d57
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-02 16:19:35 +1200

    Fill in table  details for Cart.jsx

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
 class Cart extends React.Component {
+  render () {
+    return (
+      <div className='cart'>
+        <table>
+          <thead>
+            <tr>
+              <td>Beer</td>
+              <td>Quantity</td>
+              <td>Remove</td>
+            </tr>
+          </thead>
+          <tbody>
 
+          </tbody>
+        </table>
+
+        <p className='actions'>
+          <a href='/designs/listing.html'>Continue shopping</a>
+          <button>Update</button>
+          <button className='button-primary'>Checkout</button>
+        </p>
+      </div>
+    )
+  }
 }
 
 export default Cart
