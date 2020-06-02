6e7b6dcbdcf6fff5400b996b989861cc85e4f446
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-02 14:50:36 +1200

    import BeerList component into App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import Header from './Header'
 
 import beers from '../../data/beers'
+import BeerList from './BeerList'
 
 // This might need to be turned into a stateful (class-based) component
 const App = () => (
   <div className='app'>
     <Header />
+    <BeerList beers={beers} />
   </div>
 )
 
