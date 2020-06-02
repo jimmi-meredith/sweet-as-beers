9f04bbdbda0613b3cc423f0c24295a7774311404
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-02 15:22:29 +1200

    Add comments

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Header from './Header'
 
+// importing beers data for props
 import beersObject from '../../data/beers'
 import BeerList from './BeerList'
 
@@ -9,6 +10,7 @@ import BeerList from './BeerList'
 const App = () => (
   <div className='app'>
     <Header />
+    {/* passing in the data as props (it is the full object, and beers is the array inside it) */}
     <BeerList beers={beersObject.beers}/>
   </div>
 )
--- client/components/BeerList.jsx
+++ client/components/BeerList.jsx
@@ -3,21 +3,19 @@ import React from 'react'
 import BeerListItem from './BeerListItem'
 
 const BeerList = props => {
+  // destructuring so props isn't needed to be typed each time
   const beers = props.beers
-
   return (
     <div className='beerlist'>
-
       <p className='welcome'>
         Welcome! Please select from our delicious selection and don't hesitate to let us know if we can answer any of your questions.
       </p>
-
       {beers.map(beer => {
         return (
+          // listing each beer inside props as it's own item
           <BeerListItem key={beer.id} beer={beer} />
         )
       })}
-
     </div>
   )
 }
