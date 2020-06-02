03ea02771a2f225f2fbd41504855b5b83d12dcb3
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-02 17:35:49 +1200

    Import Cart into App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,13 +5,15 @@ import Header from './Header'
 // importing beers data for props
 import beersObject from '../../data/beers'
 import BeerList from './BeerList'
+import Cart from './Cart'
 
 // This might need to be turned into a stateful (class-based) component
 const App = () => (
   <div className='app'>
     <Header />
     {/* passing in the data as props (it is the full object, and beers is the array inside it) */}
-    <BeerList beers={beersObject.beers}/>
+    {/* <BeerList beers={beersObject.beers}/> */}
+    <Cart />
   </div>
 )
 
