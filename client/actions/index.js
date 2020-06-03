5f3d74a648d2055f9ef472991a0cda5961b3c932
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-03 16:31:03 +1200

    Add comments in navigate action

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,8 +1,10 @@
+// Setting type to not equal a string
 export const NAVIGATE = 'NAVIGATE'
 
 export const navigate = target => {
   return {
     type: NAVIGATE,
+    // target being the page to show depending on the sate
     target // 'beerList' or 'cart'
   }
 }
