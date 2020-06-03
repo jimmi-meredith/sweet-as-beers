39616d6533823c69bfbcd8f58d93d093e44bf721
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-04 11:15:06 +1200

    add onClick for addToCart in BeerListItem.jsx

--- client/components/BeerListItem.jsx
+++ client/components/BeerListItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const BeerListItem = props => {
-  const beer = props.beer
+  const { beer, addToCart } = props
   return (
     <div className='beer'>
       <p className='name'>{beer.name}</p>
@@ -9,7 +9,7 @@ const BeerListItem = props => {
       <p>
         <span className='country'>{beer.country}</span>
         <span className='abv'>{beer.abv} abv</span>
-        <a href='/designs/cart.html' className='cart-link'>Add to cart</a>
+        <a href='#' className='cart-link' onClick={() => addToCart(beer.id)}>Add to cart</a>
       </p>
     </div>
   )
