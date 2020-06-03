9318c320e43231565f65395af9bccd98e9fccc84
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-03 16:03:37 +1200

    Put comments in CartItem.jsx

--- client/components/CartItem.jsx
+++ client/components/CartItem.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const CartItem = props => {
   return (
     <tr>
-      <td>HBIB Ginger Fusion</td>
+      {/* get name from props */}
+      <td>Beer Name</td>
+      {/* get quantity from props */}
       <td><input className='update-input' value="3" /></td>
+      {/* create delete action */}
       <td><button><span className='fa fa-trash fa-2x'></span></button></td>
     </tr>
   )
