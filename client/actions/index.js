d7c17a90f670571081a27ac4732be66988217480
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-03 20:12:48 +1200

    Write addToCart action

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,10 +1,19 @@
 // Setting type to not equal a string
 export const NAVIGATE = 'NAVIGATE'
+export const ADD_TO_CART = 'ADD_TO_CART'
 
 export const navigate = target => {
   return {
     type: NAVIGATE,
     // target being the page to show depending on the sate
     target // 'beerList' or 'cart'
+  }
+}
+
+export const addToCart = (id, name) => {
+  return {
+    type: ADD_TO_CART,
+    id,
+    name
   }
 }
