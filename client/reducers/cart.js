85385816a76c577db72ffaad0e7c9d3c59124a3b
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-04 10:19:06 +1200

    change addToCart reducer to use getNewCart function

--- client/reducers/cart.js
+++ client/reducers/cart.js
@@ -4,7 +4,9 @@ const addToCart = (state = [], action) => {
   switch (action.type) {
     case ADD_TO_CART:
       // returns an array with the current state, plus a new object including the details of item added
-      return [...state, { id: action.id, name: action.name, quantity: 1 }]
+      return getNewCart(state, action.id)
+    default:
+      return state
   }
 }
 
