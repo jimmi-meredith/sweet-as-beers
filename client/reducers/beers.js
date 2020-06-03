452b2067e1e8b2f6b50fd49a111722d92d0db3c9
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-04 10:37:56 +1200

    Add comments in beers reducer

--- client/reducers/beers.js
+++ client/reducers/beers.js
@@ -3,6 +3,7 @@ import beersData from '../../data/beers'
 // making the beers array avaialable as initialState
 const initialState = beersData.beers
 
+// setting the beers initialstate data to be used as global state from the redux store
 const beers = (state = initialState, action) => {
   switch (action.type) {
     default:
