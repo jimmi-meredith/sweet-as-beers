b2bd7ff36bd80e2c8e179bdec2990020d32575e7
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-04 10:37:15 +1200

    Fix error in fat arrow function

--- client/reducers/beers.js
+++ client/reducers/beers.js
@@ -1,8 +1,9 @@
 import beersData from '../../data/beers'
 
+// making the beers array avaialable as initialState
 const initialState = beersData.beers
 
-const beers = (state = initialState, action) {
+const beers = (state = initialState, action) => {
   switch (action.type) {
     default:
       return state
