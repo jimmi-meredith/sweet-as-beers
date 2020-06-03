8e67f92c2e73c3aaffd478435201e62f6387270c
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-03 18:47:15 +1200

    add comments in combined reducers.js

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 
 import currentPage from './navigation'
 
+// import all reducers so that they can all be accessed from one combined reducer
 export default combineReducers({
   currentPage
 })
