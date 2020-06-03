058671f732a8d5dcac21c441bda8244d19da95bb
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-03 20:05:21 +1200

    add comments in App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -19,10 +19,14 @@ const App = props => (
   </div>
 )
 
+// receviving the state from the redux store
 const mapStateToProps = state => {
+  // returns an object
   return {
+    // returning the currentPage state to be used as props in this component
     currentPage: state.currentPage
   }
 }
 
+// connecting this App component to the redux store
 export default connect(mapStateToProps)(App)
