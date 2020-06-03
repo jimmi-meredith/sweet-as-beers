e96dc7e831bf798707839df472b1eafdbc11d2cf
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-03 16:36:56 +1200

    add comments in navigation reducer

--- client/reducers/navigation.js
+++ client/reducers/navigation.js
@@ -1,7 +1,9 @@
 export const NAVIGATE = 'NAVIGATE'
 
+// setting the default state to be the 'beerList' page
 const currentPage = (state = 'beerList', action) => {
   switch (action.type) {
+    // if the action called is NAVIGATE, then update the target to change the state in the store
     case NAVIGATE:
       return action.target
     default:
