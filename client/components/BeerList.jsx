1e8bf4dfc66917d1b767e733cb28acbdb5087d50
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-05 14:48:46 +1200

    dispatch navigate action to 'Go to Cart' link

--- client/components/BeerList.jsx
+++ client/components/BeerList.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 // importing addToCart action for mapDispatchToProps
-import { addToCart } from '../actions'
+import { addToCart, navigate } from '../actions'
 
 import BeerListItem from './BeerListItem'
 
 const BeerList = props => {
   // destructuring so props isn't needed to be typed each time
-  const { beers, addToCart } = props
+  const { beers, addToCart, navigate } = props
   return (
     <div className='beerlist'>
       <p className='welcome'>
         Welcome! Please select from our delicious selection and don't hesitate to let us know if we can answer any of your questions.
       </p>
-      <p><a href='#'>Go to Cart</a></p>
+      <p><a href='#' onClick={() => navigate('cart')}>Go to Cart</a></p>
       {beers.map(beer => {
         return (
           // listing each beer inside props as it's own item
@@ -38,9 +38,14 @@ const mapDispatchToProps = dispatch => {
     // accessing the addToCart action to use
     addToCart: (id) => {
       dispatch(addToCart(id))
+    },
+    navigate: () => {
+      dispatch(navigate('cart'))
     }
   }
 }
+
+// PLAY WITH ADDING DISPATCH NAVIGATION HERE VS. IN APP FOR GO TO CART
 
 // connecting this component to the redux store
 export default connect(mapStateToProps, mapDispatchToProps)(BeerList)
