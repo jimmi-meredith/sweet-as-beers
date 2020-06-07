b56c9ccc930d1084a7b77313d92a7f8ea966209a
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-08 11:19:28 +1200

    add comments in CartItem component

--- client/components/CartItem.jsx
+++ client/components/CartItem.jsx
@@ -6,9 +6,8 @@ const CartItem = props => {
   const beerName = getBeerNameFromId(props.item.id)
   return (
     <tr>
-      {/* get name from props */}
+      {/* Use beer name instead of props */}
       <td>{beerName}</td>
-      {/* get quantity from props */}
       <td><input className='update-input' value={props.item.quantity} /></td>
       {/* create delete action */}
       <td><button><span className='fa fa-trash fa-2x'></span></button></td>
@@ -16,6 +15,7 @@ const CartItem = props => {
   )
 }
 
+// use id from props to match it with the beername in the data
 function getBeerNameFromId (id) {
   const beer = beersData.beers.find(beer => beer.id === id)
   return beer.name
