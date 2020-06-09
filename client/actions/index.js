4a413b731d252803db5836889cb787794bed81bf
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-10 10:22:27 +1200

    write updateQuantities action

--- client/actions/index.js
+++ client/actions/index.js
@@ -2,6 +2,7 @@
 export const NAVIGATE = 'NAVIGATE'
 export const ADD_TO_CART = 'ADD_TO_CART'
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
+export const UPDATE_QUANTITIES = 'UPDATE_QUANTITIES'
 
 export const navigate = target => {
   return {
@@ -20,9 +21,16 @@ export const addToCart = (id, name) => {
   }
 }
 
-export const removeFromCart = (id) => {
+export const removeFromCart = id => {
   return {
     type: REMOVE_FROM_CART,
     id
+  }
+}
+
+export const updateQuantities = cart => {
+  return {
+    type: UPDATE_QUANTITIES,
+    cart
   }
 }
