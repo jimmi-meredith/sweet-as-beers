f760945a1197aea085249cfa9333e2b017dbf046
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-11 10:35:56 +1200

    add home button in checkout

--- client/components/Checkout.jsx
+++ client/components/Checkout.jsx
@@ -6,6 +6,7 @@ export const Checkout = () => {
       <p className='welcome'>
         Thank you for your purchase! We hope you enjoy your order.
       </p>
+      <button className='button-primary'>Return Home</button>
     </div>
   )
 }
