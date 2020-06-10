046ae78101b784a166c3a227a93e1a80079e9358
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-11 10:28:27 +1200

    add thank you text in checkout page

--- client/components/Checkout.jsx
+++ client/components/Checkout.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export const Checkout = () => {
   return (
     <div className='checkout'>
-
+      <p className='welcome'>
+        Thank you for your purchase! We hope you enjoy your order.
+      </p>
     </div>
   )
 }
