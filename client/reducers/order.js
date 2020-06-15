44a5e5775e4a699bdaf455508cc31221b7b905a6
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-16 10:50:11 +1200

    create order reducer for adding orders

--- client/reducers/order.js
+++ client/reducers/order.js
@@ -7,6 +7,20 @@ const initialState = {
 }
 
 const order = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ORDER:
+      return {
+        ...state,
+        pending: [...state.pending,
+          {
+            id: state.pending.length + 1,
+            order: action.cart
+          }
+        ]
+      }
+    default:
+      return state
+  }
 }
 
 export default order
