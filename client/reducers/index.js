9af59123bd35369c74902dfe17e7a959b554ab47
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-16 10:53:24 +1200

    add order reducer to combined reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux'
 import currentPage from './navigation'
 import beers from './beers'
 import cart from './cart'
+import order from './order'
 
 // import all reducers so that they can all be accessed from one combined reducer
 export default combineReducers({
   currentPage,
   beers,
-  cart
+  cart,
+  order
 })
