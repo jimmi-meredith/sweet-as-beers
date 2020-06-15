d0582436f5f2fbba592f4e9c6b3bd8b537ea89e5
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-16 10:45:49 +1200

    begin order reducer

---
+++ client/reducers/order.js
@@ -0,0 +1,12 @@
+import { ADD_ORDER } from '../actions'
+
+const initialState = {
+  pending: [],
+  fulfilled: [],
+  cancelled: []
+}
+
+const order = (state = initialState, action) => {
+}
+
+export default order
