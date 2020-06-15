67140dfbe91839d557dfdff8981b20f3f04d34a7
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-16 10:52:52 +1200

    add comments in order reducer

--- client/reducers/order.js
+++ client/reducers/order.js
@@ -1,5 +1,6 @@
 import { ADD_ORDER } from '../actions'
 
+// defining initial state as an array for pending orders to be added to once an order has been placed
 const initialState = {
   pending: []
 }
@@ -9,7 +10,9 @@ const order = (state = initialState, action) => {
     case ADD_ORDER:
       return {
         ...state,
+        // adds an order to the pending array
         pending: [...state.pending,
+          // adds an object with an id, and the order info from the cart
           {
             id: state.pending.length + 1,
             order: action.cart
