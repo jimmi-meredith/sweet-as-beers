9fb5e1f76a763f39be8fd324cbdf336c2dc1f6c6
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-17 09:24:56 +1200

    add text in admin portal

--- client/components/Admin.jsx
+++ client/components/Admin.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Admin = () => {
   return (
-    <div className='admin-section'>
+    <div className='admin'>
+
+      <p calssName='welcome'>Manage orders here</p>
+
       <h3>Orders Pending</h3>
+
     </div>
   )
 }
