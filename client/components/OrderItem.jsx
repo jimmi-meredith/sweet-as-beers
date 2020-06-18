ff1e0d6331e0bc624bbae6d1c3fe777ee42f6822
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-18 15:59:53 +1200

    display order number in orderItem

--- client/components/OrderItem.jsx
+++ client/components/OrderItem.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const orderItem = () => {
-
+const orderItem = props => {
+  return (
+    <div className='order-item'>
+      <p>Order {props.id}</p>
+    </div>
+  )
 }
 
 export default orderItem
