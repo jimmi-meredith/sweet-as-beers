d126c2301ccc368ec8a5f5325c729888236593d5
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-23 10:44:02 +1200

    rename onClick to handleClick

--- client/components/OrderItem.jsx
+++ client/components/OrderItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const orderItem = props => {
   return (
-    <div className='order-item' onClick={props.onClick}>
+    <div className='order-item' onClick={props.handleClick}>
       <p>Order #{props.order.id}</p>
       {/* <p>Items Ordered = {props.pendingOrder.order.length}</p> */}
     </div>
